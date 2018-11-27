9ba254458aed4df97c39abe936329f382962729c
ionawright <ionawright@live.com>
2018-11-27 16:58:50 +0000

    ice cream stall added

---
+++ src/test/java/IceCreamStallTest.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class IceCreamStallTest {
+
+    IceCreamStall iceCreamStall;
+
+    @Before
+    public void before() {
+        iceCreamStall = new IceCreamStall("Ice Ice Baby", "Joey", 12);
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Ice Ice Baby", iceCreamStall.getName());
+    }
+
+    @Test
+    public void canGetOwnerName() {
+        assertEquals("Joey", iceCreamStall.getOwnerName());
+    }
+
+    @Test
+    public void canGetParkingSpot() {
+        assertEquals(12, iceCreamStall.getParkingSpot());
+    }
+}
